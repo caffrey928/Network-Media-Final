2bc9a78b4c2689a824c736fd96ce64b51b4233fb
caffrey928 <caffreyacnologialawace@gmail.com>
2023-05-31 19:07:54 +0800

    move rpi-functions files to server

--- rpi_server/server/utils.py
+++ rpi_server/server/utils.py
@@ -1,3 +1,5 @@
+from demo_lcd import lcd
+
 def process_data(data):
     # Process the data and return the result
     # Replace this with your own logic
@@ -6,6 +8,7 @@ def process_data(data):
 def process_payment(data):
     # Process the data and return the result
     if data["value"] >= 10:
+        lcd()
         return "Payment Success!"
     else:
         return "Payment Fail!"
